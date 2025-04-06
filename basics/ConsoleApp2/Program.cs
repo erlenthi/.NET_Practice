f5eaad25fd491cb0e1a0539010c3a502a8ba1ea8
erlenthi <erlendthingnes@outlook.com>
2025-04-06 20:19:54 +0200

    Basic C# for/if/lists

--- basics/ConsoleApp2/Program.cs
+++ basics/ConsoleApp2/Program.cs
@@ -3,4 +3,43 @@ int b = 6;
 int c = a + b;
 Console.WriteLine(c);
 
-int c = a - b;
+int c2 = a / b;
+
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The range of double is {min} to {max}");
+
+double radius = 2.50;
+
+Console.WriteLine(radius*radius*Math.PI);
+
+//https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/branches-and-loops
+
+a = 5;
+b = 3;
+if (a + b > 10)
+    Console.WriteLine("The answer is greater than 10");
+else
+    Console.WriteLine("The answer is not greater than 10");
+
+for (int row = 1; row < 11; row++)
+{
+    for (char column = 'a'; column < 'k'; column++)
+    {
+        Console.WriteLine($"The cell is ({row}, {column})");
+    }
+}
+
+int counter = 0;
+do
+{
+    Console.WriteLine($"Hello World! The counter is {counter}");
+    counter++;
+} while (counter < 10);
+
+for (int i = 0; i < 21; i++)
+{
+   if(i % 3 == 0){
+    Console.WriteLine(i);
+   }
+}
---
+++ basics/ListsIntro/Program.cs
@@ -0,0 +1,59 @@
+﻿List<string> names = ["<name>", "Ana", "Felipe"];
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+Console.WriteLine();
+names.Add("Maria");
+names.Add("Bill");
+names.Remove("Ana");
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+Console.WriteLine($"My name is {names[0]}.");
+Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
+
+Console.WriteLine($"The list has {names.Count} people in it");
+
+var index = names.IndexOf("Felipe");
+if (index == -1)
+{
+    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+}
+else
+{
+    Console.WriteLine($"The name {names[index]} is at index {index}");
+}
+
+index = names.IndexOf("Not Found");
+if (index == -1)
+{
+    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+}
+else
+{
+    Console.WriteLine($"The name {names[index]} is at index {index}");
+}
+
+names.Sort();
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+List<int> fibonacciNumbers = [1, 1];
+
+for(int i = 2; i < 20; i++){
+    var previous = fibonacciNumbers[i - 1];
+    var previous2 = fibonacciNumbers[i - 2];
+
+    fibonacciNumbers.Add(previous + previous2);
+}
+
+foreach (var number in fibonacciNumbers)
+{
+    Console.WriteLine(number);
+}
+Console.WriteLine(fibonacciNumbers);
